b0b93c048c2ac5fb9563ce54d03f361f36988f9a
LINETH-03 <lperezh9@miumg.edu.gt>
2021-02-24 00:17:18 -0600

    change name to file

---
+++ holamundo.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace hello_world
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hola mundooo!");
+            Console.WriteLine("Lily Lineth");
+            
+         
+        }
+    }
+}
